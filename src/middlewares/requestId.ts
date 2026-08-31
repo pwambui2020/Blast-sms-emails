6807f7b3afef888022357d9ce5761bb150a84251
Lneema2026 <paulynewambui95@gmail.com>
2026-08-31 20:31:22 +0300

    added unique ID for each request

---
+++ src/middlewares/requestId.ts
@@ -0,0 +1,15 @@
+import {Request,Response, NextFunction} from "express";
+import {randomUUID} from "crypto";
+
+
+export function requestId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const id =randomUUID();
+    
+    req.headers["x-request-id"] = id;
+    res.setHeader("x-request-id", id);
+    next();
+}
