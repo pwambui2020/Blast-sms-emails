c4b8189575e0077c6f8d0404b6309fa0570894b1
Lneema2026 <paulynewambui95@gmail.com>
2026-08-31 16:24:31 +0300

    add the asyncHandler and apiResponse helper functions

---
+++ src/utils/apiResponse.ts
@@ -0,0 +1,27 @@
+import { Response } from "express";
+
+export function successResponse<T>(
+  res: Response,
+  message: string,
+  data?: T,
+  statusCode = 200
+) {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+}
+
+export function errorResponse(
+  res: Response,
+  message: string,
+  errors?: unknown,
+  statusCode = 400
+) {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors,
+  });
+}
