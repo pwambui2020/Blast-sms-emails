ce31104410bb5d15782a009b50fff20f5ec338ad
Lneema2026 <paulynewambui95@gmail.com>
2026-09-04 15:23:39 +0300

    Update server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
 import app from "./app";
 import { env } from "./config/env";
-import { logger } from "./config/logger";
+import { logger } from "./config/logger"; 
+import { prisma } from "./config/prisma";
 
-app.listen(env.PORT, () => {
-  logger.info(
-    `🚀 Server running on http://localhost:${env.PORT}`
-  );
-});
+async function startServer() {
+  try {
+    await prisma.$connect();
+
+    logger.info("Connected to the database");
+
+    app.listen(env.PORT, () => {
+      logger.info(
+        `Server running on http://localhost:${env.PORT}`
+    );
+    });
+
+  } catch (error) {
+    logger.error(error,"Error connecting to the database");
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  }
+}
+
+startServer();
