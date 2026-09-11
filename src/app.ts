f9b51e5220d1ef085f3b1514a0e369dec9a7381d
Lneema2026 <paulynewambui95@gmail.com>
2026-09-11 13:12:06 +0300

    Register health and API documentation routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+
 import compression from "compression";
 import { notFound } from "./middlewares/notFound";
 import { errorHandler } from "./middlewares/errorHandler";
 import {requestLogger} from "./middlewares/requestLogger";
 import { apiLimiter } from "./middlewares/rateLimiter";
+
 import healthRoutes from "./routes/health.routes"
+import { swaggerUi, swaggerDocument } from "./config/swagger";
 
 const app = express();
 
@@ -18,14 +21,20 @@ app.use(compression()); //compress responses
 
 app.use(express.json()); //parse JSON request bodies
 
+app.use(requestLogger);
+
+app.use(apiLimiter);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
+
 app.use("/api/v1/health", healthRoutes);
 
 app.use(notFound);
 
 app.use(errorHandler);
-
-app.use(requestLogger);
-
-app.use(apiLimiter);
 
 export default app;
